7baca6f1e4bf63aa7435de944039baf9aa8157d4
Riley Williams <williams.riley@icloud.com>
2021-04-05 22:19:55 -0400

    Adds enabled checks to TappableElement

--- Sources/Beady/TappableElement.swift
+++ Sources/Beady/TappableElement.swift
@@ -10,19 +10,35 @@ import XCTest
 
 public protocol TappableElement: InterfaceElement {
 	static func tap(withNumberOfTaps: Int, numberOfTouches: Int) throws
+	static func isEnabled() throws
 }
 
 public extension TappableElement {
 	static func tap(withNumberOfTaps taps: Int = 1, numberOfTouches touches: Int = 1) throws {
+		try isEnabled()
+		
 		let element = try locate()
-		guard element.isHittable, element.isEnabled else {
-			throw TappableElementError.expectedEnabled
-		}
 		#if os(macOS)
 		element.tap()
 		#else
 		element.tap(withNumberOfTaps: taps, numberOfTouches: touches)
 		#endif
+	}
+	/// Throws if the element is not enabled
+	static func isEnabled() throws {
+		let element = try locate()
+		guard element.isHittable, element.isEnabled else {
+			throw TappableElementError.expectedEnabled
+		}
+	}
+}
+
+public extension TappableElement {
+	/// Throws if the element is enabled
+	static func isNotEnabled() throws {
+		guard (try? isEnabled()) == nil else {
+			throw TappableElementError.expectedDisabled
+		}
 	}
 }
 
